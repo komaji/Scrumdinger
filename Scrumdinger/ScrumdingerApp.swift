cc2802a9c3e302c85a79b99659a7e0879b70bc6a
komaji <don.kojikoji@gmail.com>
2021-01-04 16:02:00 +0900

    git init

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by kojima.t on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
