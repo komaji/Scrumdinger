7ea6771051a8bd985574829f1ec9dba1b0207f4f
komaji <don.kojikoji@gmail.com>
2021-01-04 20:22:38 +0900

    Section4 終わり

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -33,6 +33,15 @@ struct DetailView: View {
                 }
                 .accessibilityElement(children: .ignore)
             }
+
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label("\(attendee)", systemImage: "person")
+                        .foregroundColor(.black)
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text("\(attendee)"))
+                }
+            }
         }
         .listStyle(InsetGroupedListStyle())
     }
