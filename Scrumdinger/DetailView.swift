fbba17e1d10d26768090aa718b5a94beb1f7d065
komaji <don.kojikoji@gmail.com>
2021-01-05 16:26:35 +0900

    Section5 終わり

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
 
+    @State private var isPresented = false
+
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -46,7 +48,24 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView()
+                    .navigationBarItems(
+                        leading: Button("Cancel") {
+                            isPresented = false
+                        },
+                        trailing: Button("Done") {
+                            isPresented = false
+                        }
+                    )
+                    .navigationTitle(scrum.title)
+            }
+        }
     }
 }
 
