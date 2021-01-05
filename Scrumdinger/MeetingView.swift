3c797295668d9f1baeb7ea9208abd292bc4bbee7
komaji <don.kojikoji@gmail.com>
2021-01-05 18:51:56 +0900

    Section6 終わり

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -24,14 +24,7 @@ struct MeetingView: View {
                 )
                 Circle()
                     .strokeBorder(lineWidth: 24.0, antialiased: true)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel(Text("Next speaker"))
-                }
+                MeetingFooterView(speakers: $scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
